c8d5779b08034c0839c1558f2a223587ab2ad434
Johan Kristian Candra <44829050+johancandra@users.noreply.github.com>
2021-10-22 15:39:13 +0700

    Update Article.vue

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -3,7 +3,8 @@
     <div><img class="image" :alt=title :src=image></div>
     <div>{{ title }}</div>
     <div>published_at : {{ published_at }}</div>
-    <div>expired_at : {{ expired_at }}</div>
+    <!--<div>expired_at : {{ expired_at }}</div>-->
+    <div>expired_at : {{ namahari2[(namahari.indexOf(published_at)+1+expired_at)%7] }}</div>
 </div>
 </template>
 
@@ -15,7 +16,20 @@ export default {
     title : String,
     published_at : String,
     expired_at : Number
-  }
+  },
+  afterMount(){
+    this.hari = this.namahari.indexOf(this.published_at) + 1
+    this.hari = this.hari+this.expired_date
+    this.hari = this.hari % 7
+    this.hari = this.namahari[this.hari]
+  },
+  data() {
+    return {
+        namahari: ["Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"],
+        namahari2: ["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"],
+        hari:0,
+    }
+  },
 }
 </script>
 
